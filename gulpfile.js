0386c619892fa9eec4dcb3ca03def4cb7837aa69
Julian Rosenblum <jsrmath@gmail.com>
2016-11-28 19:43:26 -0500

    Set up build system

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var del = require('del');
+var babel = require('babelify');
+
+gulp.task('build', function () {
+  return browserify('./scripts/main.jsx', {debug: true})
+    .transform(babel.configure({presets: ["react", "es2015"]}))
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('clean', function () {
+  return del(['bundle.js']);
+});
+
+gulp.task('default', ['clean', 'build']);
